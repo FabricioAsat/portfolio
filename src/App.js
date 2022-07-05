29aaa5095b6b2d6fc67971df4fffde4ad0b4e29e
Fabricio Asat <fabricioasat00@gmail.com>
2022-07-05 17:06:21 -0300

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Proyects from "./components/Proyects/Proyects";
 
 function App() {
 	useEffect(() => {
-		Aos.init({ once: true, disable: "mobile" });
+		Aos.init({ once: true });
 	}, []);
 
 	return (
